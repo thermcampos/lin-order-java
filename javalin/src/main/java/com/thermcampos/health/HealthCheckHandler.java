eafdb7d9959ea05c63e97a063653a04f886914f0
Ricardo Campos <ricardompcampos@gmail.com>
2026-09-07 19:10:30 +0200

    chore: wire new jsonutil class to health api

--- javalin/src/main/java/com/thermcampos/health/HealthCheckHandler.java
+++ javalin/src/main/java/com/thermcampos/health/HealthCheckHandler.java
@@ -1,5 +1,7 @@
 package com.thermcampos.health;
 
+import com.thermcampos.util.JsonUtil;
+import java.util.Map;
 import io.javalin.http.ContentType;
 import io.javalin.http.Context;
 import io.javalin.http.HttpStatus;
@@ -9,6 +11,6 @@ public class HealthCheckHandler {
     public void getHealth(Context ctx) {
         ctx.addHeader(ContentType.APPLICATION_JSON.name(), ContentType.APPLICATION_JSON.getMimeType());
         ctx.status(HttpStatus.OK);
-        ctx.result("{\"status\":\"OK\"}");
+        ctx.result(JsonUtil.toJson(Map.of("status", "UP")));
     }
 }
